ed992551729f54e482a7fb0e50e72e182d90f928
BoFo <brunofazio466@gmail.com>
2023-11-20 15:38:41 -0300

    modelos de la base de datos mapeados

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'homebanking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'C:/Users/brufa\Documents/ITBA/sprint6/itbank.db ',
     }
 }
 
