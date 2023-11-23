ea2893c190ec47a28edf559b31c1787aecc18bd0
Santiago Carle <Fastest037@users.noreply.github.com>
2023-11-22 21:06:43 -0300

    tercera problematica terminada, FUTUROS TEMPLATES Heredan de base.html y usar @login_required en las views

--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Login.views import LoginView, LogoutView, RegisterView, HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('register/', RegisterView.as_view(),name='register'),
+    path('login/', LoginView.as_view(),name='login'),
+    path('logout/', LogoutView.as_view(),name='logout'),
 ]
